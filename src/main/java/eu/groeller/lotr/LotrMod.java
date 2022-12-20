dc71190219859b34b27c9613f721a8daaf3ef9bf
GroellerKarim <karim.jonas.groeller@gmail.com>
2022-12-20 14:15:15 +0100

    Added gondor shield

--- src/main/java/eu/groeller/lotr/LotrMod.java
+++ src/main/java/eu/groeller/lotr/LotrMod.java
@@ -1,6 +1,13 @@
 package eu.groeller.lotr;
 
+import eu.groeller.lotr.items.GondorShieldItem;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.item.Items;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -10,12 +17,16 @@ public class LotrMod implements ModInitializer {
 	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
 
+
+	public static final Item GONDOR_SHIELD = new GondorShieldItem(new FabricItemSettings().maxDamage(2500), 10, 13, Items.NETHERITE_INGOT);
+
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
-
 		LOGGER.info("Hello Fabric world!");
+
+		Registry.register(Registries.ITEM, new Identifier("lotrme", "gondor_shield"), GONDOR_SHIELD);
 	}
 }
--- src/main/java/eu/groeller/lotr/items/GondorShieldItem.java
+++ src/main/java/eu/groeller/lotr/items/GondorShieldItem.java
@@ -1,4 +1,13 @@
 package eu.groeller.lotr.items;
 
-public class GondorShieldItem {
+import com.github.crimsondawn45.fabricshieldlib.lib.object.FabricShieldItem;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.item.Items;
+import net.minecraft.item.ToolMaterial;
+
+public class GondorShieldItem extends FabricShieldItem {
+    public GondorShieldItem(Settings settings, int cooldownTicks, int enchantability, Item... repairItems) {
+        super(settings, cooldownTicks, enchantability, repairItems);
+    }
 }
