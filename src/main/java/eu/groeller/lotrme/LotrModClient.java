ec971bb391b3a190a84e233b080677c98b2ee897
GroellerKarim <karim.jonas.groeller@gmail.com>
2023-02-03 23:33:54 +0100

    Initial Commit - Project Setup
    Adding the basic fabric modding setup we're going to use
    Dependencies like the fabric shield dependency will probably still be
    added. Time to get to work :))
    
    	- Changed Modid to "lotrme"
    	- Added geckolib4 dependency
    
    Changes to be committed:
    	new file:   .github/workflows/build.yml
    	new file:   .gitignore
    	new file:   README.md
    	new file:   build.gradle
    	new file:   gradle.properties
    	new file:   gradle/wrapper/gradle-wrapper.jar
    	new file:   gradle/wrapper/gradle-wrapper.properties
    	new file:   gradlew
    	new file:   gradlew.bat
    	new file:   settings.gradle
    	new file:   src/main/java/eu/groeller/lotrme/LotrMod.java
    	new file:   src/main/java/eu/groeller/lotrme/LotrModClient.java
    	new file:   src/main/java/eu/groeller/lotrme/mixin/ExampleMixin.java
    	new file:   src/main/resources/fabric.mod.json
    	new file:   src/main/resources/lotrme.mixins.json
    
    Signed-Off-By: Karim Jonas Groeller <groeller@kekx.eu>

---
+++ src/main/java/eu/groeller/lotrme/LotrModClient.java
@@ -0,0 +1,11 @@
+package eu.groeller.lotrme;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LotrModClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/eu/groeller/lotrme/mixin/ExampleMixin.java
@@ -0,0 +1,16 @@
+package eu.groeller.lotrme.mixin;
+
+import eu.groeller.lotrme.LotrMod;
+import net.minecraft.client.gui.screen.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TitleScreen.class)
+public class ExampleMixin {
+	@Inject(at = @At("HEAD"), method = "init()V")
+	private void init(CallbackInfo info) {
+		LotrMod.LOGGER.info("This line is printed by an example mod mixin!");
+	}
+}
